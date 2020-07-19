d15e4f084b8223c9d3c98b7572b8ab2f17370403
Verendless <Hadisutarma12@Gmail.com>
2020-07-19 20:55:21 +0700

    Add Player Movement Boundry & Player's Leser

---
+++ Space Shooter/Assets/Scripts/Leser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leser : MonoBehaviour
+{
+    private float _speed = 8.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        LeserBehaviour();
+    }
+
+    void LeserBehaviour()
+    {
+        transform.Translate(Vector2.up * _speed * Time.deltaTime);
+
+        if(transform.position.y >= 7.9f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Space Shooter/Assets/Scripts/Player.cs
+++ Space Shooter/Assets/Scripts/Player.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private float _speed = 5.0f;
-    private float _verticalInput;
+    [SerializeField] 
+    private GameObject _leserPrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +16,30 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculatePlayerMovement();
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_leserPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+    void CalculatePlayerMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector2 direction = new Vector2(horizontalInput, verticalInput);
         transform.Translate(direction * _speed * Time.deltaTime);
+        
+        transform.position = new Vector2(transform.position.x, Mathf.Clamp(transform.position.y, -3.0f, 0));
+
+        if(transform.position.x <= -11.0f)
+        {
+            transform.position = new Vector2(11.0f, transform.position.y);
+        }
+        else if(transform.position.x >= 11.0f)
+        {
+            transform.position = new Vector2(-11.0f, transform.position.y);
+        }
     }
 }
